4ea181fdd704c0a07b8befa7c4e26d06d06b558b
Ashish Prajapati <ashishyuvasoft107@gmail.com>
2015-08-05 19:42:15 +0530

    After signup path redirect to sign in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,19 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    params[:user][:password]=12345678
+    params[:user][:password]=generated_password
     devise_parameter_sanitizer.for(:sign_up)
+  end
+
+  def generated_password
+  	generated_password = Devise.friendly_token.first(8)
+  end
+
+  def sign_up(resource_name, resource)
+    true
+  end
+
+  def after_sign_up_path_for(resource)
+    new_user_session_path
   end
 end

16a833204f75723cf663a02370812e5d9448b803
Ashish Prajapati <ashishyuvasoft107@gmail.com>
2016-02-15 17:56:55 +0530

    Added prooper product image path

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,8 +1,5 @@
 class Gallery < ActiveRecord::Base
-	has_attached_file :photo, :styles => { :small => "150x150>" },
-                  :url  => "/assets/products/:id/:style/:basename.:extension",
-                  :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
-
+	has_attached_file :photo, :styles => { :small => "150x150>" }
 	validates_attachment_presence :photo
 	validates_attachment_size :photo, :less_than => 5.megabytes
 	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']

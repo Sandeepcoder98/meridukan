4030e4ae2c503f6a7ed07613458c39caaf9f4827
Ashish Prajapati <ashishyuvasoft107@gmail.com>
2016-03-14 13:49:09 +0530

    Features implemented for images

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,5 @@
 class Gallery < ActiveRecord::Base
-	has_attached_file :photo, :styles => { :small => "150x150>" }
+	has_attached_file :photo, :styles => { :small => "150x150>" }, :default_url => '/assets/missing_photo_:style.png'
 	validates_attachment_presence :photo
 	validates_attachment_size :photo, :less_than => 5.megabytes
 	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']

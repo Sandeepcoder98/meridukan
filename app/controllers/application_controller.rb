5529845aca900879aca2ceda02a1498c777240e0
Ashish Prajapati <ashishyuvasoft107@gmail.com>
2016-03-14 11:52:36 +0530

    JS Fix of ckeditor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,7 @@ class ApplicationController < ActionController::Base
  
   layout :layout_by_resource
   helper_method :current_order
-
-  def after_sign_in_path_for(resource)
-      authenticated_root_path
-  end
-
+  
   def current_order
     begin
       if !session[:order_id].nil?

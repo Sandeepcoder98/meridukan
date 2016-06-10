691764a478e5dd8872d203eed05eef8735443981
vaibhav <vaibhavyuvasoft122@gmail.com>
2016-06-10 19:12:14 +0530

    made seller dashboard page dynamic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,7 +14,10 @@ class DashboardController < ApplicationController
 	def buyer_request
 	end
 
-	def my_products
+	def my_products 
+		@products_approved_false = current_user.products.where(apply_approve: false)
+		@products_approved = current_user.products.where(apply_approve: true)
+		@products_denied = current_user.products.where(approve: false)
 	end
 
 	def analytics
